e089cf06c9ccc8fc41f583fa9078a8c9eeaccf07
Edwin Olson <eblson@umich.edu>
2014-01-22 22:57:04 -0500

    add pnm write to image_u8x3

--- src/common/image_u8x3.c
+++ src/common/image_u8x3.c
@@ -5,7 +5,9 @@
 
 #include "image_u8x3.h"
 
-#define DEFAULT_ALIGNMENT 72
+// least common multiple of 32 (cache line) and 24 (stride needed for
+// 8byte-wide RGB processing). (It's possible that 24 would be enough).
+#define DEFAULT_ALIGNMENT 96
 
 image_u8x3_t *image_u8x3_create(int width, int height)
 {
@@ -40,3 +42,29 @@ void image_u8x3_destroy(image_u8x3_t *im)
     free(im);
 }
 
+int image_u8x3_write_pnm(const image_u8x3_t *im, const char *path)
+{
+    FILE *f = fopen(path, "wb");
+    int res = 0;
+
+    if (f == NULL) {
+        res = -1;
+        goto finish;
+    }
+
+    // Only outputs to RGB
+    fprintf(f, "P6\n%d %d\n255\n", im->width, im->height);
+    int linesz = im->width * 3;
+    for (int y = 0; y < im->height; y++) {
+        if (linesz != fwrite(&im->buf[y*im->stride], 1, linesz, f)) {
+            res = -1;
+            goto finish;
+        }
+    }
+
+finish:
+    if (f != NULL)
+        fclose(f);
+
+    return res;
+}
--- src/common/image_u8x3.h
+++ src/common/image_u8x3.h
@@ -30,6 +30,8 @@ image_u8x3_t *image_u8x3_create(int width, int height);
 // force stride to be a multiple of 'alignment' bytes.
 image_u8x3_t *image_u8x3_create_alignment(int width, int height, int alignment);
 
+    int image_u8x3_write_pnm(const image_u8x3_t *im, const char *path);
+
 void image_u8x3_destroy(image_u8x3_t *im);
 
 #ifdef __cplusplus
