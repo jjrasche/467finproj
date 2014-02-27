35eb710a7e15964fe503e9e8fca5e5f833d5a780
Shane DeMeulenaere <shanemd@umich.edu>
2014-02-27 17:17:36 -0500

    motor_feedback_test update

--- src/maebot/motor_feedback_test.c
+++ src/maebot/motor_feedback_test.c
@@ -7,9 +7,13 @@ lcm_t* lcm;
 static void
 motor_feedback_handler(const lcm_recv_buf_t *rbuf, const char* channel, const maebot_motor_feedback_t* msg, void* user)
 {
-    printf("%lld,\t%d,\t\t%d\n", msg->utime,
-           msg->encoder_left_ticks,
-           msg->encoder_right_ticks);
+    system("clear");
+    printf("utime: %lld\n", msg->utime);
+    printf("Subscribed to channed: MAEBOT_MOTOR_FEEDBACK");
+    printf("encoder_[left, right]_ticks: %d,\t%d\n", msg->encoder_left_ticks, msg->encoder_right_ticks);
+    printf("motor_current[left, right]: %d,\t%d\n", msg->motor_current_left, msg->motor_current_right);
+    printf("motor_[left, right]_commanded_speed: %f,\t%f\n", msg->motor_left_commanded_speed, msg->motor_right_commanded_speed);
+    printf("motor_[left, right]_actual_speed: %f,\t%f\n", msg->motor_left_commanded_speed, msg->motor_right_commanded_speed);
 }
 
 int main()
@@ -32,5 +36,3 @@ int main()
 
     return 0;
 }
-
-
